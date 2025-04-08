995628c21731c61f254c94f1f7285e2b58fe3f64
Stuart Gendron <stu.gendron@gmail.com>
2025-04-08 14:04:56 -0700

    project init

---
+++ project-01/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  Serial.println("Blink!");
+}
